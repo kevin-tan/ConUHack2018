e3a6e7dbe22adf0c3847c0227ffddb612215891f
kevin tan <kevin.tan97@hotmail.com>
2018-01-27 18:24:53 -0500

    added clockthread fixed overhead of resource

--- src/main/java/com/program/Program.java
+++ src/main/java/com/program/Program.java
@@ -1,13 +1,12 @@
 package com.program;
 
+import com.program.window.ClockThread;
 import com.program.window.ProgramWindow;
 import com.startup.constants.WeekdayId;
 import com.startup.data.DataRepository;
 import com.startup.model.Courses;
 import com.windows.constants.FrameConstants;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -19,11 +18,11 @@ public class Program {
 
     public Program() {
         programWindow = new ProgramWindow("", FrameConstants.INFO_WINDOW_WIDTH, FrameConstants.INFO_WINDOW_HEIGHT);
-        dateThread = new Thread(timeDateThread(programWindow));
+        dateThread = new ClockThread(programWindow);
         dataRepo = DataRepository.getDataRepository();
     }
 
-    public void start(){
+    public void start() {
         programWindow.setVisible(true);
         dateThread.start();
     }
@@ -31,19 +30,5 @@ public class Program {
     public void stop() throws InterruptedException {
         dateThread.join();
         programWindow.setVisible(false);
-    }
-
-    private Runnable timeDateThread(final ProgramWindow programWindow){
-        return () -> {
-            while(programWindow.isVisible()){
-                programWindow.setTitle(getDateAndTime());
-            }
-        };
-    }
-
-    private String getDateAndTime(){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date currrentDate = new Date();
-        return simpleDateFormat.format(currrentDate);
     }
 }
---
+++ src/main/java/com/program/window/ClockThread.java
@@ -0,0 +1,33 @@
+package com.program.window;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ClockThread extends Thread{
+
+    private boolean isRunning;
+    private final ProgramWindow programWindow;
+
+    public ClockThread(ProgramWindow programWindow){
+        this.programWindow = programWindow;
+    }
+
+    @Override
+    public void run(){
+        while(programWindow.isVisible()){
+            try {
+                programWindow.setTitle(getDateAndTime());
+                this.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getDateAndTime() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date currrentDate = new Date();
+        return simpleDateFormat.format(currrentDate);
+    }
+
+}
