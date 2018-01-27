acd81b455218c2693d60b530f4b646e3448a454d
kevin tan <kevin.tan97@hotmail.com>
2018-01-27 17:08:29 -0500

    setting up startupform

--- src/main/java/com/startup/StartupForm.java
+++ src/main/java/com/startup/StartupForm.java
@@ -1,4 +1,13 @@
 package com.startup;
 
+import com.startup.form.Form;
+import com.startup.window.StartupWindow;
+
 public class StartupForm {
+
+    public StartupForm(Form form){
+        StartupWindow startupWindow; //TODO complete
+
+    }
+
 }
