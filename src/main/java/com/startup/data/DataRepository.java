f55d6b348ca0fb46e2565a01e182f1c0d35b603a
kevin tan <kevin.tan97@hotmail.com>
2018-01-27 16:57:23 -0500

    fixed repo, added time and courses model

--- src/main/java/com/startup/data/DataRepository.java
+++ src/main/java/com/startup/data/DataRepository.java
@@ -1,7 +1,12 @@
 package com.startup.data;
 
 import com.startup.constants.WeekdayId;
+import com.startup.model.Courses;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataRepository {
-    private Map<WeekdayId, List<>>
+    private Map<WeekdayId, List<Courses>> dataRepository = new HashMap<>();
 }
--- src/main/java/com/startup/model/Courses.java
+++ src/main/java/com/startup/model/Courses.java
@@ -1,4 +1,19 @@
 package com.startup.model;
 
 public class Courses {
+    private final String courseName;
+    private final Time time;
+
+    public Courses(String courseName, Time time){
+        this.courseName = courseName;
+        this.time = time;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Time getTime() {
+        return time;
+    }
 }
