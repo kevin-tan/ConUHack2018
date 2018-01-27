74ed2e85015a157d4f693651d8beabb08f93842a
kevin tan <kevin.tan97@hotmail.com>
2018-01-27 16:44:17 -0500

    adding data repo

---
+++ src/main/java/com/startup/data/DataRepository.java
@@ -0,0 +1,4 @@
+package com.startup.data;
+
+public class DataRepository {
+}
--- src/main/java/com/startup/form/Form.java
+++ src/main/java/com/startup/form/Form.java
@@ -1,10 +1,11 @@
 package com.startup.form;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Form extends JPanel{
 
     public Form(){
-
+        setLayout(new GridLayout(5,2));
     }
 }
