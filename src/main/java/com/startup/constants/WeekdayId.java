377d22352ca0074c9b4ef8b2841d04c6fa6820f4
Sagar <patelsagar1997@gmail.com>
2018-01-27 17:52:21 -0500

    added first form with weekdays and number of departures

--- src/main/java/com/startup/constants/WeekdayId.java
+++ src/main/java/com/startup/constants/WeekdayId.java
@@ -1,9 +1,19 @@
 package com.startup.constants;
 
 public enum WeekdayId {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday");
+
+    private final String weekday;
+
+    WeekdayId(String weekday) {
+        this.weekday = weekday;
+    }
+
+    public String getWeekday() {
+        return weekday;
+    }
 }
--- src/main/java/com/startup/form/DeparturesForm.java
+++ src/main/java/com/startup/form/DeparturesForm.java
@@ -1,20 +1,48 @@
 package com.startup.form;
 
+import com.startup.constants.WeekdayId;
+import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static com.windows.constants.FrameConstants.INFO_WINDOW_WIDTH;
 import static com.windows.constants.FrameConstants.INFO_WINDOW_HEIGHT;
 
 public class DeparturesForm extends JPanel {
 
+    private JButton submitButton;
+
+
     public DeparturesForm() {
         setSize(INFO_WINDOW_WIDTH, INFO_WINDOW_HEIGHT);
-        setLayout(new MigLayout("insets 5"));
-        setBackground(Color.GREEN);
+        setLayout(new MigLayout(new LC().wrapAfter(2).insets("5")));
+        setBackground(Color.BLACK);
         setVisible(true);
+
+        submitButton = new JButton("Submit");
+
+        submitButton.addActionListener((actionEvent) -> System.out.println("button pressed"));
+
+        addTextboxes();
+        add(submitButton);
+    }
+
+    private void addTextboxes() {
+        for (WeekdayId weekdayId : WeekdayId.values()) {
+            JLabel weekdayLabel = new JLabel(weekdayId.getWeekday() + ": ");
+            weekdayLabel.setForeground(Color.WHITE);
+            add(weekdayLabel);
+            JTextArea jTextArea = new JTextArea(1, 10);
+            add(jTextArea);
+        }
+    }
+
+    private void registerData() {
+
     }
 }
