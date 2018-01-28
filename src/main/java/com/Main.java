3204bc1ac1c999de653e6804f5bbae6fc310723e
Sagar <patelsagar1997@gmail.com>
2018-01-28 03:28:24 -0500

    added WebLookAndFeel

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,16 +1,22 @@
 package com;
 
 
+import com.alee.laf.WebLookAndFeel;
 import com.data.DataConstants;
 import com.startup.StartupForm;
 
+import javax.swing.*;
 import java.io.File;
 
 public class Main {
 
 
-
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new WebLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         StartupForm startupForm = new StartupForm();
         startupForm.startStartupForm();
         File file = new File(DataConstants.FILE_DATA);
