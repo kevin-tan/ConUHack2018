9a95069d6c6deea9eeac228d6912c9c57f6d4dba
Sagar <patelsagar1997@gmail.com>
2018-01-27 19:59:08 -0500

    changed textarea to a combobox (better for desired input)

--- src/main/java/com/startup/data/DataConstants.java
+++ src/main/java/com/startup/data/DataConstants.java
@@ -2,4 +2,5 @@ package com.startup.data;
 
 public abstract class DataConstants {
     public static final String FILE_DATA = "C:\\Users\\Kevin Programming\\IdeaProjects\\hackathonProj\\src\\main\\resources\\data.txt";
+    public static final Integer[] NUM_OF_DEPARTURES = {1, 2, 3};
 }
--- src/main/java/com/startup/form/DeparturesForm.java
+++ src/main/java/com/startup/form/DeparturesForm.java
@@ -7,19 +7,17 @@ import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
-
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.windows.constants.FrameConstants.INFO_WINDOW_WIDTH;
+import static com.startup.data.DataConstants.NUM_OF_DEPARTURES;
 import static com.windows.constants.FrameConstants.INFO_WINDOW_HEIGHT;
+import static com.windows.constants.FrameConstants.INFO_WINDOW_WIDTH;
 
 public class DeparturesForm extends JPanel {
 
-    private JTextArea[] jTextAreas = new JTextArea[5];
-
+    private JComboBox[] numOfDepartures = new JComboBox[5];
 
     public DeparturesForm() {
         setSize(INFO_WINDOW_WIDTH, INFO_WINDOW_HEIGHT);
@@ -31,18 +29,18 @@ public class DeparturesForm extends JPanel {
 
         submitButton.addActionListener((actionEvent) -> registerData());
 
-        addTextboxes();
+        addComboBoxes();
         add(submitButton);
     }
 
-    private void addTextboxes() {
+    private void addComboBoxes() {
         int index = 0;
         for (WeekdayId weekdayId : WeekdayId.values()) {
             JLabel weekdayLabel = new JLabel(weekdayId.getWeekday() + ": ");
             weekdayLabel.setForeground(Color.WHITE);
             add(weekdayLabel);
-            jTextAreas[index] = new JTextArea(1, 10);
-            add(jTextAreas[index]);
+            numOfDepartures[index] = new JComboBox<>(NUM_OF_DEPARTURES);
+            add(numOfDepartures[index]);
             index++;
         }
     }
@@ -51,8 +49,7 @@ public class DeparturesForm extends JPanel {
         int index = 0;
         for (WeekdayId weekdayId : WeekdayId.values()) {
             List<Courses> courses = new ArrayList<>();
-            //TODO what about invalid input? (wrong type)
-            int numberOfDepartures = Integer.parseInt(jTextAreas[index].getText());
+            int numberOfDepartures = (int) numOfDepartures[index].getSelectedItem();
             for (int i = 0; i < numberOfDepartures; i++) {
                 courses.add(new Courses());
             }
